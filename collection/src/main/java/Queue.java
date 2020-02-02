6d08b011c54dcf8542acff26e4121ceec2e93a98
Vinay <vinay.shivani@gmail.com>
2020-02-02 10:35:13 +0800

    Queue logic with working test cases

--- collection/src/main/java/Queue.java
+++ collection/src/main/java/Queue.java
@@ -1,30 +1,77 @@
 import java.util.Optional;
 
-public class Queue {
+public class Queue<V> {
     private int capacity=100;
-    private Object[] ringBuffer;
-    private int start=-1;       //add elements from this point
-    private int end=-1;         //consume elements from this point.
+    private V[] ringBuffer;
+    private int start=0;       //add elements from this point
+    private int end=0;         //consume elements from this point.
     private int size=0;
+
 
     public Queue(int capacity){
         this.capacity = capacity;
-        ringBuffer= new Object[capacity];
+        init();
     }
+
+
     public Queue(){
-        ringBuffer = new Object[capacity];
+        init();
     }
 
-    public void enqueue(Object elem){
-
+    private void init(){
+        //noinspection MoveFieldAssignmentToInitializer
+        ringBuffer= (V[]) new Object[capacity];
     }
 
-    public Optional<Object> dequeue(){
+    public void enqueue(V elem){
+        if(isFull()){
+            upSizeBuffer();
+        }
+        insertElem(elem);
+    }
+
+    private void insertElem(V elem){
+        if(start == capacity){
+            start = 0;
+        }
+        ringBuffer[start++] = elem;
+        size++;
+    }
+
+    private void upSizeBuffer() {
+        capacity = capacity * 2;    //TODO - gaurd against overflow.
+        V[] newBuffer = (V[]) new Object[capacity];
+        int i=0;
+        for (V item : ringBuffer){
+            newBuffer[i++] = item;
+        }
+        ringBuffer = newBuffer;
+    }
+
+    private boolean isFull() {
+        return size == capacity;
+    }
+
+    public Optional<V> dequeue(){
+        if (size > 0){
+            return removeElem();
+        }
         return Optional.empty();
     }
 
-    public Optional<Object> peek(){
-        return end < 0 ? Optional.empty() : (Optional<Object>) ringBuffer[end];
+    private Optional<V> removeElem() {
+        if(end == capacity)
+            end = 0;
+
+        size--;
+        return Optional.of(ringBuffer[end++]);
+    }
+
+    public Optional<V> peek(){
+        if(end == capacity)
+            end = 0;
+
+        return size == 0 ? Optional.empty() : Optional.of(ringBuffer[end]);
     }
 
     public int size(){
